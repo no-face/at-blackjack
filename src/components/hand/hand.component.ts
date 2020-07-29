18ff5043ab3b21289a4a966789cb26334ea446b6
Alex <alexandria.wolanski@gmail.com>
2020-07-29 15:43:50 -0400

    Add hit and stand methods for player hand

--- src/components/hand/hand.component.ts
+++ src/components/hand/hand.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 import { Card } from '../../services/helpers';
 import { GameService } from '../../services/game.service';
@@ -8,13 +8,17 @@ import { GameService } from '../../services/game.service';
   templateUrl: './hand.component.html',
   styleUrls: ['./hand.component.scss']
 })
-export class HandComponent implements OnInit {
+export class HandComponent {
   @Input() hand: Array<Card> = [];
   @Input() player: string = 'dealer';
 
-  constructor(public gameService: GameService) {}
+  constructor(public gameService: GameService) { }
 
-  ngOnInit(): void {
+  hit(): void {
+    this.gameService.drawCard(this.player);
+  }
 
+  stand(): void {
+    this.gameService.end();
   }
 }
--- src/services/game.service.ts
+++ src/services/game.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Observable, BehaviorSubject } from 'rxjs';
-import { filter, mergeMap, map, scan } from 'rxjs/operators';
+import { Observable, BehaviorSubject, Subject } from 'rxjs';
+import { filter, mergeMap, map, scan, takeUntil } from 'rxjs/operators';
 
 import { deck, shuffle, Card } from './helpers';
 
@@ -8,6 +8,7 @@ import { deck, shuffle, Card } from './helpers';
 export class GameService {
   private deck: BehaviorSubject<Array<Card>> = new BehaviorSubject(shuffle(deck));
   private deckPosition: BehaviorSubject<{ player: string; position: number }> = new BehaviorSubject({ player: 'unknown', position: null });
+  private endGame: Subject<void> = new Subject();
 
   deck$: Observable<Card[]> = this.deck.asObservable();
 
@@ -20,6 +21,10 @@ export class GameService {
     this.deck.next(shuffle(deck));
   }
 
+  end(): void {
+    this.endGame.next();
+  }
+
   generateHand(playerName: string): Observable<Array<Card>> {
     return this.deck$.pipe(
       mergeMap((cards: Array<Card>) =>
@@ -28,9 +33,10 @@ export class GameService {
           .pipe(
             filter(({ player }) => player === playerName),
             map(({ position }) => (cards[position])),
-          )
+        )
       ),
-      scan((cards: Array<Card>, card: Card) => cards.concat(card), [])
+      scan((cards: Array<Card>, card: Card) => cards.concat(card), []),
+      takeUntil(this.endGame.asObservable())
     );
   }
 }
